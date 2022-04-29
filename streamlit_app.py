bb8a5a3658a1d9892e7f1c1f11e273aa96e5b2d3
sbadrunin <101139265+sbadrunin@users.noreply.github.com>
2022-04-29 21:49:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
-# import nltk
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
